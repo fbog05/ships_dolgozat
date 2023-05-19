0b6a3dfcca374609a15f46dee154672f88558011
ottoburg17@gmail.com <Nmeth Ott>
2023-05-19 09:36:50 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,53 @@
-//Ferencsik Boglárka
-//SZOFT-I-2-N
-//2023-05-19
+/*File: app.js
+* Author: erencsik Boglárka
+* Group: SZOFT-I-2-N
+* Date: 2023-05-19
+* Github: https://github.com/fbog05/
+* Licenc: GNU GPL
+*/
+
+const doc = {
+    tbody: document.getElementById('tbody')
+};
+
+const state = {
+    host: 'http://localhost:8000/',
+    ships: []
+};
+
+window.addEventListener('load', () => {
+    init();
+});
+
+function init(){
+    getShips();
+};
+
+function getShips(){
+    let endpoint = 'ships'
+    let url = state.host + endpoint
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+        state.ships = result
+        render();
+    });
+};
+
+function render(){
+    var rows = ''
+    state.ships.forEach( ship => {
+        var row = `
+        <tr>
+            <td>${ship.name}</td>
+            <td>${ship.length}</td>
+            <td>${ship.price}</td>
+            <td>${ship.person}</td>
+            <td>${ship.trailer}</td>
+        </tr>
+        `;
+        rows += row
+        console.log(ship.name)
+    });
+    doc.tbody.innerHTML = rows
+};
